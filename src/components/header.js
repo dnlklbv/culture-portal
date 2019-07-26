82686b91b3d432bbdf374ec72d8cdb5b1c475d92
Krystsina Gerko <krystsina-belfresh@mail.ru>
2019-07-26 22:28:10 +0300

    feat: add header's title and positioning

--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,9 @@
 import { Link } from 'gatsby';
 import PropTypes from 'prop-types';
 import React from 'react';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
 
 const Header = ({ siteTitle }) => (
   <header
@@ -9,25 +12,24 @@ const Header = ({ siteTitle }) => (
       marginBottom: '1.45rem',
     }}
   >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
+    <Container>
+      <Row>
+        <Col lg={{ span: 8, offset: 2 }}>
+          <h1 style={{ margin: 0, fontSize: '2rem' }}>
+            <Link
+              to="/"
+              style={{
+                color: 'white',
+                textDecoration: 'none',
+              }}
+            >
+              {siteTitle}
+            </Link>
+          </h1>
+          <h2 style={{ fontSize: '1.2rem' }}>Information portal</h2>
+        </Col>
+      </Row>
+    </Container>
   </header>
 );
 
@@ -36,7 +38,7 @@ Header.propTypes = {
 };
 
 Header.defaultProps = {
-  siteTitle: '',
+  siteTitle: 'Writers of Belarus',
 };
 
 export default Header;
