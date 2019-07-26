1a317e86b675352b3b933a49ab5e18b4f353ee5b
dnlklbv <hi.daniil.k@gmail.com>
2019-07-26 18:16:21 +0300

    feat: add fake portal description on main page

---
+++ src/pages/index.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import Layout from '../components/layout';
+import SEO from '../components/seo';
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" />
+    <h2>Welcome to the portal dedicated to Belarusian poets</h2>
+    <p>
+      Fugiat incididunt deserunt et sit voluptate pariatur duis id. Cupidatat
+      exercitation voluptate adipisicing esse amet do do esse id et nulla consequat fugiat
+      laborum. Non sint elit pariatur veniam elit pariatur mollit laborum adipisicing deserunt amet.
+    </p>
+    <p>
+      Lorem ullamco officia fugiat commodo occaecat aliquip proident aliquip consectetur ullamco
+      irure proident. Dolore quis id velit dolor consectetur excepteur fugiat consectetur et ut aute
+      ex pariatur id. Voluptate excepteur consectetur aute mollit. Ullamco exercitation
+      voluptate est aute dolor excepteur aute officia. Non nisi dolore ullamco in ullamco deserunt
+      dolore est. Mollit fugiat anim velit aute ea non nisi amet eiusmod commodo.
+    </p>
+  </Layout>
+);
+
+export default IndexPage;
