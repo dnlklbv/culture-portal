213c435e987892541ada382e609a74b396a347c8
Krystsina Gerko <krystsina-belfresh@mail.ru>
2019-07-26 22:18:48 +0300

    feat: change header's color, portal's title and description

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   pathPrefix: "/culture-portal",
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `Writers of Belarus`,
+    description: `Welcome to the information portal about the famous Belarusian writers.`,
     author: `@gatsbyjs`,
   },
   plugins: [
