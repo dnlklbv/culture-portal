1161a28c499ba40584e87c791d12632b819ad83e
dnlklbv <hi.daniil.k@gmail.com>
2019-07-27 00:34:36 +0300

    feat: fill cards with data

--- src/components/DevTeam/DevTeam.jsx
+++ src/components/DevTeam/DevTeam.jsx
@@ -8,11 +8,35 @@ const DevTeam = () => (
     <h2>Development team</h2>
     <CardGroup style={{ margin: 'auto' }}>
       <Card>
+        <Card.Img variant="top" src="https://github.com/andreysomov.png" />
+        <Card.Body>
+          <Card.Title>Andrey Somov</Card.Title>
+          <Card.Subtitle className="text-muted">AndreySomov</Card.Subtitle>
+          <Card.Link href="https://github.com/andreysomov" target="_blank">GitHub</Card.Link>
+        </Card.Body>
+      </Card>
+      <Card>
         <Card.Img variant="top" src="https://github.com/dnlklbv.png" />
         <Card.Body>
-          <Card.Title>Daniil Kulyabov</Card.Title>
+          <Card.Title>Daniil Kuliabau</Card.Title>
           <Card.Subtitle className="text-muted">dnlklbv</Card.Subtitle>
           <Card.Link href="https://github.com/dnlklbv" target="_blank">GitHub</Card.Link>
+        </Card.Body>
+      </Card>
+      <Card>
+        <Card.Img variant="top" src="https://github.com/grigoriyklimenko.png" />
+        <Card.Body>
+          <Card.Title>Grigoriy Klimenko</Card.Title>
+          <Card.Subtitle className="text-muted">GrigoriyKlimenko</Card.Subtitle>
+          <Card.Link href="https://github.com/grigoriyklimenko" target="_blank">GitHub</Card.Link>
+        </Card.Body>
+      </Card>
+      <Card>
+        <Card.Img variant="top" src="https://github.com/chrisgerby.png" />
+        <Card.Body>
+          <Card.Title>Krystsina Gerko</Card.Title>
+          <Card.Subtitle className="text-muted">ChrisGerBy</Card.Subtitle>
+          <Card.Link href="https://github.com/chrisgerby" target="_blank">GitHub</Card.Link>
         </Card.Body>
       </Card>
     </CardGroup>
