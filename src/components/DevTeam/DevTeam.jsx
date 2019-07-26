fca9be4ccf87bf327e1fb70c9fe8f9ffe2843ae0
dnlklbv <hi.daniil.k@gmail.com>
2019-07-27 00:50:22 +0300

    feat: implement cards rendering with array mapping

--- src/components/DevTeam/DevTeam.jsx
+++ src/components/DevTeam/DevTeam.jsx
@@ -3,44 +3,46 @@ import React from 'react';
 import Card from 'react-bootstrap/Card';
 import CardGroup from 'react-bootstrap/CardGroup';
 
-const DevTeam = () => (
-  <>
-    <h2>Development team</h2>
-    <CardGroup style={{ margin: 'auto' }}>
-      <Card>
-        <Card.Img variant="top" src="https://github.com/andreysomov.png" />
-        <Card.Body>
-          <Card.Title>Andrey Somov</Card.Title>
-          <Card.Subtitle className="text-muted">AndreySomov</Card.Subtitle>
-          <Card.Link href="https://github.com/andreysomov" target="_blank">GitHub</Card.Link>
-        </Card.Body>
-      </Card>
-      <Card>
-        <Card.Img variant="top" src="https://github.com/dnlklbv.png" />
-        <Card.Body>
-          <Card.Title>Daniil Kuliabau</Card.Title>
-          <Card.Subtitle className="text-muted">dnlklbv</Card.Subtitle>
-          <Card.Link href="https://github.com/dnlklbv" target="_blank">GitHub</Card.Link>
-        </Card.Body>
-      </Card>
-      <Card>
-        <Card.Img variant="top" src="https://github.com/grigoriyklimenko.png" />
-        <Card.Body>
-          <Card.Title>Grigoriy Klimenko</Card.Title>
-          <Card.Subtitle className="text-muted">GrigoriyKlimenko</Card.Subtitle>
-          <Card.Link href="https://github.com/grigoriyklimenko" target="_blank">GitHub</Card.Link>
-        </Card.Body>
-      </Card>
-      <Card>
-        <Card.Img variant="top" src="https://github.com/chrisgerby.png" />
-        <Card.Body>
-          <Card.Title>Krystsina Gerko</Card.Title>
-          <Card.Subtitle className="text-muted">ChrisGerBy</Card.Subtitle>
-          <Card.Link href="https://github.com/chrisgerby" target="_blank">GitHub</Card.Link>
-        </Card.Body>
-      </Card>
-    </CardGroup>
-  </>
-);
+const DevTeam = () => {
+  const ghURL = 'https://github.com/';
+  const developers = [
+    {
+      name: 'Andrey Somov',
+      username: 'AndreySomov',
+    },
+    {
+      name: 'Daniil Kuliabau',
+      username: 'dnlklbv',
+    },
+    {
+      name: 'Grigoriy Klimenko',
+      username: 'GrigoriyKlimenko',
+    },
+    {
+      name: 'Krystsina Gerko',
+      username: 'ChrisGerBy',
+    },
+  ];
+
+  const developersCards = developers.map(developer => (
+    <Card key={developer.username}>
+      <Card.Img variant="top" src={`${ghURL + developer.username}.png`} />
+      <Card.Body>
+        <Card.Title>{developer.name}</Card.Title>
+        <Card.Subtitle className="text-muted">{developer.username}</Card.Subtitle>
+        <Card.Link href={ghURL + developer.username} target="_blank">GitHub</Card.Link>
+      </Card.Body>
+    </Card>
+  ));
+
+  return (
+    <>
+      <h2>Development team</h2>
+      <CardGroup>
+        {developersCards}
+      </CardGroup>
+    </>
+  );
+};
 
 export default DevTeam;
