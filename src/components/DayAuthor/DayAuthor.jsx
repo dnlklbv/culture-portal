615dd583c804a6290c2e219260466cf04027fc3b
GrigoryKlimenko <fageol46@gmail.com>
2019-07-28 17:53:23 +0300

    feat: add data to fill author of the day card

--- src/components/DayAuthor/DayAuthor.jsx
+++ src/components/DayAuthor/DayAuthor.jsx
@@ -5,7 +5,20 @@ import Col from 'react-bootstrap/Col';
 import Card from 'react-bootstrap/Card';
 
 const DayAuthor = () => {
-  const authors = [];
+  const authors = [
+    {
+      name: 'Maksim Adamavič Bahdanovič',
+      img: 'https://www.syl.ru/misc/i/ai/289376/1586801.jpg',
+      date: '27 November (9 December) 1891 – 12 (25) May 1917',
+      shortDescription: 'Was a Belarusian poet, journalist, translator, literary critic and historian of literature. He is considered as one of the founders of the modern Belarusian literature.',
+    },
+    {
+      name: 'Yanka Kupala (Iván Daminíkavich Lutsévich)',
+      img: 'https://24smi.org/public/media/resize/800x-/celebrity/2018/12/06/ofy1rxpievtc-ianka-kupala.jpg',
+      date: '7 July (25 June) 1882 – 28 June 1942',
+      shortDescription: 'A Belarussian poet and writer. Kupala is considered one of the greatest Belarusian-language writers of the 20th century.',
+    },
+  ];
   const randomAuthor = (authorsList) => {
     const randomIndex = Math.floor(Math.random() * authorsList.length);
     return authorsList[randomIndex];
