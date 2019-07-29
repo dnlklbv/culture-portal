e5830b3697d7cb18c42d1991870fefd7f0bb7aa9
dnlklbv <hi.daniil.k@gmail.com>
2019-07-29 16:50:16 +0300

    feat: query data on poets page

--- src/pages/poets.jsx
+++ src/pages/poets.jsx
@@ -1,16 +1,57 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
+import { graphql } from 'gatsby';
 import Layout from '../components/layout';
 import SEO from '../components/seo';
 
 import ListOfAuthors from '../components/ListOfAuthors';
 
-const SecondPage = () => (
-  <Layout>
-    <SEO title="Poets" />
-    <h2 className="mb-4">Poets</h2>
-    <ListOfAuthors />
-  </Layout>
-);
+const PoetsPage = ({ data }) => {
+  const { edges } = data.allJavascriptFrontmatter;
+  return (
+    <Layout>
+      <SEO title="Poets" />
+      <h2 className="mb-4">Poets</h2>
+      <ListOfAuthors authors={edges} />
+    </Layout>
+  );
+};
 
-export default SecondPage;
+PoetsPage.propTypes = {
+  data: PropTypes.shape({
+    allJavascriptFrontmatter: PropTypes.shape({
+      edges: PropTypes.arrayOf(PropTypes.shape({
+        node: PropTypes.shape({
+          id: PropTypes.string,
+          frontmatter: PropTypes.shape({
+            path: PropTypes.string,
+            lng: PropTypes.string,
+            name: PropTypes.string,
+            birthPlace: PropTypes.string,
+          }),
+        }),
+      })),
+    }),
+  }).isRequired,
+};
+
+export const pageQuery = graphql`
+  query {
+    allJavascriptFrontmatter {
+      edges {
+        node {
+          id
+          frontmatter {
+            lng
+            path
+            birthPlace
+            name
+          }
+        }
+      }
+    }
+  }
+`;
+
+export default PoetsPage;
