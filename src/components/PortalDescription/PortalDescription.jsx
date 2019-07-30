8618f5ef7b748d7e66472136a6d2dc491c472815
Krystsina Gerko <krystsina-belfresh@mail.ru>
2019-07-30 20:27:02 +0300

    feat: create PortalDescription component

---
+++ src/components/PortalDescription/PortalDescription.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const PortalDescription = () => (
+  <div>
+  <h2
+      style={{ textAlign: 'center' }}>
+        Welcome to the information portal about the poets of Belarus!
+    </h2>
+    <p>
+      On this website you can find:
+      <ul>
+        <li>
+          the biography of the authors that describes the most important stages of their  lives,
+        </li>
+        <li>
+          the maps of memorable places connected with their life and work,
+        </li>
+        <li>
+          the images and interesting videos that describe
+          the most significant pieces of authors’ works!
+        </li>
+      </ul>
+
+    </p>
+    <p>
+      Enjoy reading and exploring the lives of the most honored poets of Belarus!
+    </p>
+  </div>
+);
+
+export default PortalDescription;
