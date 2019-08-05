cab0048f0ad9853bed8f24ec100380319947f474
Krystsina Gerko <krystsina-belfresh@mail.ru>
2019-08-05 20:02:13 +0300

    feat: fill data about Pashkevich

--- src/data/Pashkevich.en.js
+++ src/data/Pashkevich.en.js
@@ -3,4 +3,53 @@ exports.frontmatter = {
   lng: 'en',
   name: 'Alaiza Pashkevich',
   birthPlace: 'Grodno',
+  birthDate: '15 July 1876',
+  deathDate: ' 5 November 1916',
+  imgSrc: 'https://upload.wikimedia.org/wikipedia/commons/7/78/Pashkevich.jpg',
+  videoId: 'GlYYLSkqG90',
+  map: [
+    {
+      lat: '53.6067679',
+      lng: '24.7394578',
+      descr: 'Памятник Алаизе Пашкевич',
+    },
+  ],
+  gallery: [
+    'https://upload.wikimedia.org/wikipedia/commons/9/93/Ałaiza_Paškievič._Алаіза_Пашкевіч_%281904%29.jpg',
+    'https://upload.wikimedia.org/wikipedia/commons/b/b4/Алаіза_Пашкевіч-Кайрыс._БДАМЛМ.jpg',
+    'https://upload.wikimedia.org/wikipedia/commons/2/29/Першае_чытанне_для_дзетак_беларусаў.jpg',
+  ],
+  works: [
+    {
+      year: '1906',
+      text: 'Сборники стихов "Хрэст на свабоду" і "Скрыпка беларуская"',
+    },
+
+  ],
+  bio: [
+    {
+      year: '1876',
+      text: ' Alaiza Pashkievich was born to a wealthy szlachta family. She graduated from Vilnius Private School V. Prozaravej. After moved to Saint Petersburg.',
+    },
+    {
+      year: '1902',
+      text: 'She graduated externally from the Gymnasium Alexandria for girls and joined a school for physical education teachers.',
+    },
+    {
+      year: '1904',
+      text: 'Alaiza gave up teaching and returned to Vilnius. She organized workers\' groups, wrote and promoted anti-government proclamations, and took part in debates and political meetings.',
+    },
+    {
+      year: '1906',
+      text: 'She published two collections of poems, “Хрэст на свабоду” and “Скрыпка беларуская” in Zhovkva. At the same time she traveled illegally to Vilnius, where she participated in the issuance of the newspaper Nasha Dola.',
+    },
+    {
+      year: '1911',
+      text: 'She married Steponas Kairys, a Lithuanian engineer and social democracy activist. In the same year, she returned to Belarus and joined national educational activities.',
+    },
+    {
+      year: '1916',
+      text: 'Alaiza Pashkievich traveled to her parents and helped villagers sick with typhoid. She also fell ill with typhus and died 5 February.',
+    },
+  ],
 };
