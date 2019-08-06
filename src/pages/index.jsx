79b3cdd51e84a65c67a495c9ee4c8c0fd38a62fc
dnlklbv <hi.daniil.k@gmail.com>
2019-08-06 13:03:25 +0300

    feat: improve day author

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
+import { graphql } from 'gatsby';
 
 import Layout from '../components/layout';
 import SEO from '../components/seo';
@@ -7,13 +10,53 @@ import DayAuthor from '../components/DayAuthor';
 import DevTeam from '../components/DevTeam';
 import PortalDescription from '../components/PortalDescription';
 
-const IndexPage = () => (
+const IndexPage = ({ data }) => (
   <Layout>
     <SEO title="Home" />
     <PortalDescription />
-    <DayAuthor />
+    <DayAuthor authors={data.allJavascriptFrontmatter.edges} />
     <DevTeam />
   </Layout>
 );
+
+IndexPage.propTypes = {
+  data: PropTypes.shape({
+    allJavascriptFrontmatter: PropTypes.object,
+  }).isRequired,
+};
+
+export const pageQuery = graphql`
+  query {
+    allJavascriptFrontmatter {
+      edges {
+        node {
+          frontmatter{
+            en {
+              path
+              imgSrc
+              birthDate
+              deathDate
+              name
+            }
+            ru {
+              path
+              imgSrc
+              birthDate
+              deathDate
+              name
+            }
+            by {
+              path
+              imgSrc
+              birthDate
+              deathDate
+              name
+            }
+          }
+        }
+      }
+    }
+  }
+`;
 
 export default IndexPage;
